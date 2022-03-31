44a28c78db710142d8b870be985aeb54ba5d4e79
rpottier <rpottier@student.42.fr>
2022-03-31 19:27:56 +0200

    read_line

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -3,14 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rpottier <rpottier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 17:24:05 by bsavinel          #+#    #+#             */
-/*   Updated: 2022/03/31 17:30:32 by bsavinel         ###   ########.fr       */
+/*   Updated: 2022/03/31 17:56:42 by rpottier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*get_command_line(char *buffer)
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main(int argc, const char * argv[])
+{
+	char *user_input;
+
+	user_input = readline(NULL);
+	parser(user_input);
+	return 0;
+}
+
+void parser(char *user_input)
 {
 	
 }
+
+
