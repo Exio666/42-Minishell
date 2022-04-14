2d8adb186afa90b87c3dd17c793b4fa8a56b03d5
Benoit Savinel <bsavinel@student.42.fr>
2022-04-14 14:29:02 +0200

    checker pipe sequence fini mais pas teste

---
+++ srcs/checker/pipe_sequence_checker.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe_sequence_checker.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/14 12:09:11 by bsavinel          #+#    #+#             */
+/*   Updated: 2022/04/14 13:38:15 by bsavinel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int len_no_whitespace(char *commande)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (str[i])
+	{
+		i++;
+		if (!ft_iswhitespace(str[i]))
+			len++;
+	}	
+	return (len);
+}
+
+int	pipe_sequence_checker(char *commande)
+{
+	t_checker	check;
+
+	init_struct_checker(&check);
+	if (len_no_whitespace(commande) == 0)
+	{
+		check.error = TRUE;
+		check.str = S_ERROR_MISSING_COMMANDE;
+	}
+	if (check.error == FALSE)
+		redirection_checker(commande, &check)
+	if (check.error == TRUE)
+		printf("%s\n", check.str);
+	return (!check.error);
+}
---
+++ srcs/checker/redirection_checker.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirection_checker.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/14 11:35:16 by bsavinel          #+#    #+#             */
+/*   Updated: 2022/04/14 14:08:47 by bsavinel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	redirect_out_checker(char *commande, t_checker *check)
+{
+	check->index++;
+	if (commande[check->index] && commande[check->index] == '>')
+	{
+		check->index++;
+		else if (commande[check->index] == '>')
+		{
+			check->str = S_ERROR_UNEX_TOK_REDIRECT_IN;
+			check->error = TRUE;
+		}
+	}
+	while (commande[check->index] && ft_iswhitespace(commande[check->index]) 
+			&& check->error == FALSE)
+		check->index++;
+	if (commande[check->index] && commande[check->index] != '>' 
+			&& commande[check->index] != '<' && commande[check->index] != '|')
+		check->arg_of_redirect = TRUE;
+	if (check->arg_of_redirect == FALSE)
+	{
+		check->str = S_ERROR_MIS_ARG_REDIRECT_OUT;
+		check->error = TRUE;
+	}
+}
+
+void	redirect_in_checker(char *commande, t_checker *check)
+{
+	check->index++;
+	if (commande[check->index] && commande[check->index] == '<')
+	{
+		check->index++;
+		if (commande[check->index] == '<')
+		{
+			check->str = S_ERROR_UNEX_TOK_REDIRECT_IN;
+			check->error = TRUE;
+		}
+	}
+	while (commande[check->index] && ft_iswhitespace(commande[check->index]) 
+			&& check->error == FALSE)
+		check->index++;
+	if (commande[check->index] && commande[check->index] != '>' 
+			&& commande[check->index] != '<' && commande[check->index] != '|')
+		check->arg_of_redirect = TRUE;
+	if (check->arg_of_redirect == FALSE)
+	{
+		check->str = S_ERROR_MIS_ARG_REDIRECT_IN;
+		check->error = TRUE;
+	}
+}
+
+void	pipe_content_checker(char *commande, t_checker *check)
+{
+	int	i;
+	int len;
+	
+	check->index++;
+	i = check->index;
+	len = 0;
+	while (commande[i] && commande[i] != '|')
+	{
+		if (!ft_iswhitespace(commande[i]))
+			len++;
+		i++;
+	}
+	if (len == 0)
+		check->error = TRUE;
+}
+
+int	redirection_checker(char *pipe_sequence, t_checker *check)
+{
+	while (pipe_sequence[check->index])
+	{
+		check->arg_of_redirect = FALSE;
+		if (pipe_sequence[check->index] == '>')
+			redirect_out_checker(pipe_sequence, check);
+		else if (pipe_sequence[check->index] == '<')
+			redirect_in_checker(pipe_sequence, check);
+		else if (pipe_sequence[check->index] == '|')
+			pipe_content_checker(pipe_sequence, check);
+		if (check->error == TRUE)
+			return (0);
+		check->index = jump_caracters(pipe_sequence, "\"\'<>|", check->index, 1);
+		quote_jump(pipe_sequence, check);
+	}
+	return (1);
+}
