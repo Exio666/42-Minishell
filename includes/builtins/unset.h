c633ca8dfa02da6db538f8580668ef14f8f7c84e
Benoit Savinel <bsavinel@student.42.fr>
2022-05-09 16:33:57 +0200

    unset ajout d'un checker

--- includes/builtins/unset.h
+++ includes/builtins/unset.h
@@ -6,7 +6,7 @@
 /*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 14:34:21 by bsavinel          #+#    #+#             */
-/*   Updated: 2022/05/03 16:43:11 by bsavinel         ###   ########.fr       */
+/*   Updated: 2022/05/09 16:28:23 by bsavinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 int			ft_unset(int ac, char **arg, t_lst_env **envp);
 int			delete_varraible(char *name, t_lst_env **envp);
+int			check_arg_unset(char *arg);
 t_lst_env	*find_element_with_name(char *name, t_lst_env **envp);
 
 #endif
