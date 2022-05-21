b3439ebdee725e758d817bf6f172761926c5e0a2
Rodolphe Pottier <rodolphegamer@hotmail.fr>
2022-05-21 09:45:38 +0200

    bug quote, check a faire

--- srcs/execution/main_execution.c
+++ srcs/execution/main_execution.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main_execution.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rpottier <rpottier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 20:35:13 by rpottier          #+#    #+#             */
-/*   Updated: 2022/05/17 17:46:55 by bsavinel         ###   ########.fr       */
+/*   Updated: 2022/05/21 09:32:37 by rpottier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(int argc, char **argv, char **envp)
 {
 	char		*command_line;
 	t_lst_env	**env_list;
-	const char	prompt[] = GRN "Bsavinel_and_associates_shell$ " RESET;
+	const char	prompt[] = "Bsavinel_and_associates_shell$ ";
 
 	(void)(argc);
 	(void)(argv);
--- srcs/expand/expand_functions.c
+++ srcs/expand/expand_functions.c
@@ -6,7 +6,7 @@
 /*   By: rpottier <rpottier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 12:39:00 by rpottier          #+#    #+#             */
-/*   Updated: 2022/05/21 01:17:32 by rpottier         ###   ########.fr       */
+/*   Updated: 2022/05/21 09:44:29 by rpottier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,9 @@ t_lst_token	*expand_token(t_lst_token *token, t_lst_env *env_list)
 	if (ft_strlen(token->str) == 0)
 		return (token);
 	token = expand_all_variables(token, env_list);
+
+	printf("[%s]\n", token->str);
+	print_lstquote(token->lst_quote);
 	token->str = remove_quotes(token, token->lst_quote);
 	return (token);
 }
@@ -50,15 +53,17 @@ t_lst_token	*expand_all_variables(t_lst_token *token, t_lst_env *env_list)
 	t_quote_index	quote;
 	t_lst_quote		*elem;
 int tmp = -2;
-	int max = 15;
+//	int max = 15;
 	i = 0;
-	while (i != -1 && token && token->str[i] && max)
+	while (i != -1 && token && token->str[i] /*&& max*/)
 	{
+
+	tmp = -2;
 		quote.open = -1;
 		quote.close = - 1;
-		printf("i = %d\n", i);
-		printf("%s\n", token->str);
-		max--;
+		// printf("i = %d\n", i);
+		// printf("%s\n", token->str);
+		//max--;
 		if (i != -1 && token->str[i] && is_quote(token->str[i]))
 		{
 			quote.open = i;
@@ -75,10 +80,12 @@ int tmp = -2;
 			else if (i != -1 && token->str[i] && is_dollar(token->str[i]) && token->str[i + 1])
 				tmp = i;
 
-		printf("%s\n", token->str);
-printf("i -> %d\n", i);
-printf("tmp -> %d\n", tmp);
+	
 		}
+ 		printf("\n\n%s\n", token->str);
+		printf("i -> %d\n", i);
+		printf("tmp -> %d\n", tmp);
+		printf("quote.open = %d \n quote.close =%d\n\n", quote.open, quote.close);
 		if (i != -1 && token && token->str && token->str[i] && !is_quote(token->str[i]) && tmp != i)
 		{
 	//		printf("bonjour\n");
@@ -94,8 +101,8 @@ printf("tmp -> %d\n", tmp);
 		else if (i != -1 && token && token->str && token->str[i] && is_wildcard(token->str[i]))
 			i++;
 	}
-	if (max == 0)
-		exit(1);
+	//  if (max == 0)
+	//  	exit(1);
 	return (token);
 }
 
@@ -130,7 +137,7 @@ char	*expand_variable(char *token, int *index, t_lst_env *env_list)
 
 	insert_var_content_to_token(&token, var_content, *index);
 
-	if(var_content[0] && !(var_content_len <= 1))
+	if(var_content[0] && !(var_content_len < 1))
 		*index = *index + (var_content_len - 1);
 	else
 		*index = *index + (var_content_len);
